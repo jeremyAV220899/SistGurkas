e3ae4a7c16715553e5e95f9c0d50bc4b42792783
jeremy apaico villena <jeremy232129@hotmail.com>
2026-09-15 20:26:34 -0500

    Agregar archivos de proyecto.

---
+++ SistGurkas/Controllers/Acceso/AccesoController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistGurkas.Models.Acceso;
+
+namespace SistGurkas.Controllers.Acceso
+{
+    public class AccesoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(Usuario _usuario)
+        {
+            return RedirectToAction("Index", "Home");
+
+        }
+    }
+}
---
+++ SistGurkas/Models/Acceso/Usuario.cs
@@ -0,0 +1,8 @@
+﻿namespace SistGurkas.Models.Acceso
+{
+    public class Usuario
+    {
+        public string Correo { get; set; }
+        public string Clave { get; set; }
+    }
+}
